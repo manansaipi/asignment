ef68bc2f002703807909a9f6eb794e4ebec5d003
manansaipi <98406415+manansaipi@users.noreply.github.com>
2022-04-03 20:36:48 +0700

    Update centerPage.php

--- project/centerPage.php
+++ project/centerPage.php
@@ -11,7 +11,7 @@ $articel = query("SELECT * FROM articel");
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Mini articel</title>
+    <title>Mini Blog</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styleCPage.css">
 </head>
